425c580571bcbf8df614cabd7eaee8491c64c26d
zhangwei8k <8kemail@163.com>
2017-10-24 16:17:57 +0800

    2017-10-24

---
+++ resources/app/client/src/js/action.js
@@ -0,0 +1,20 @@
+///////////////////////////////////// Action ///////////////////////////////////////////
+//run 启动入口
+Action.run = function(page, wait, fn){
+    $("#Loader").css("visibility", "visible");
+    setTimeout(function(){
+        Room.ppt({id:["Loader" , page] , mov:["mv-fade-out" , "mv-fade-in"]}, function(){
+            if(fn) fn();
+        });
+    }, wait);
+};
+
+Action.Nav = function(){
+    Room.ppt({id:[Room.id, "Nav"] , mov:["fadeOut" , "fadeIn"]});
+};
+Action.Video = function(id){
+    Room.ppt({id:[Room.id, "Video"] , mov:["fadeOut" , "fadeIn"]});
+};
+Action.VideoEnd = function(id){
+    Room.ppt({id:[Room.id, "Nav"] , mov:["fadeOut" , "fadeIn"]});
+};
